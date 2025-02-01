f0c99e3e3fd408d6d955e5918770fa6373f332ef
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-01-31 23:47:53 -0300

    remove nav top

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,14 +3,13 @@ import Sidebar from '../components/sidebar';
 
 export default function Layout({ children }) {
 	return (
-		<div className="row h-100">
-			<div className="col-12">
-				<Navbar />
-			</div>
-			<div className="col-3">
+		<div className="row vh-100">
+			<div className="col-3 p-0">
 				<Sidebar />
 			</div>
-			<div className="col-8 p-4">{children}</div>
+			<div className="col-8 p-0 h-100  overflow-y-scroll p-r-0 pt-5">
+				{children}
+			</div>
 		</div>
 	);
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 function Dashboard() {
-	return <div className="h-100">Dashboard</div>;
+	return <div className="">Dashboard</div>;
 }
 
 export default Dashboard;
