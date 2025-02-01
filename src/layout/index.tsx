379b06917410dccfa550638d8a411a4b587cb716
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-01-31 23:50:57 -0300

    layout fixes

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,13 +3,11 @@ import Sidebar from '../components/sidebar';
 
 export default function Layout({ children }) {
 	return (
-		<div className="row vh-100">
-			<div className="col-3 p-0">
+		<div className="row vh-100 m-0">
+			<div className="col-2 p-0">
 				<Sidebar />
 			</div>
-			<div className="col-8 p-0 h-100  overflow-y-scroll p-r-0 pt-5">
-				{children}
-			</div>
+			<div className="col-10 p-0">{children}</div>
 		</div>
 	);
 }
