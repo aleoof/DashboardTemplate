233ba9b2690d728bc6a4ca66dbe72742a12a074c
Edson Rodrigues <shartech.solucoes@gmail.com>
2025-02-06 12:56:02 -0300

    inclusão do campo pesquisa e configurações

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,14 +1,21 @@
-import {BsHouseFill} from "react-icons/bs";
+import {BsPersonFillGear, BsHouseFill} from "react-icons/bs";
 
 export default function Navbar() {
 	return (
-		<div className="header d-flex justify-content-between">
+		<div className="header d-flex justify-content-between mb-4">
 			<div className="float-start">
 				<p><BsHouseFill /> / Dashboard</p>
 				<h4><strong>Dashboard</strong></h4>
 			</div>
-			<div className="float-end">
-
+			<div className="float-end d-flex search">
+				<input
+					type="text"
+					className="form-control nav"
+					id="name"
+					placeholder="Pesquisar"
+					value=""
+				/>
+				<a className="icons_nav"><BsPersonFillGear /></a>
 			</div>
 		</div>
 	);
