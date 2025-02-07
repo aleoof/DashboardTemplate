d0004e635695b7a66dd8866d1aa48665b7158bed
Edson Rodrigues <shartech.solucoes@gmail.com>
2025-02-07 10:19:08 -0300

    Melhorias Gerais

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,12 @@
 import {BsPersonFillGear, BsHouseFill} from "react-icons/bs";
+import {useLocation} from "react-router";
 
 export default function Navbar() {
+	const { pathname } = useLocation();
 	return (
 		<div className="header d-flex justify-content-between mb-4">
 			<div className="float-start">
-				<p><BsHouseFill /> / Dashboard</p>
+				<p><BsHouseFill /> { pathname }</p>
 				<h4><strong>Dashboard</strong></h4>
 			</div>
 			<div className="float-end d-flex search">
