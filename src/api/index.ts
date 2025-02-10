98e484c268c4149fdd060066d2da63a2c1688591
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-02-10 16:04:33 -0300

    fix the os field

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const secret = localStorage.getItem('token');
 
 export const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || 'https://api.shartech.com.br/',
+	baseURL: import.meta.env.VITE_API_URL,
 	headers: { Authorization: `Bearer ${secret}` },
 });
