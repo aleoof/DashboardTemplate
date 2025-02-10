11aec3160e7960f32b75fa2938ef12d110c5035c
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-02-10 11:34:26 -0300

    close qr if scanned

--- src/components/QRCodeScanner/index.tsx
+++ src/components/QRCodeScanner/index.tsx
@@ -8,6 +8,12 @@ export default function QRCodeScanner({
 	closeQR: () => void;
 	handleValue: (value: string) => void;
 }) {
+	const handlerQr = (value: string) => {
+		handleValue(value);
+		if (value) {
+			closeQR();
+		}
+	};
 	return (
 		<div className="position-fixed h-100 w-100 top-0 start-0 d-flex flex-column align-items-center justify-content-center bg-dark">
 			<button
@@ -20,11 +26,11 @@ export default function QRCodeScanner({
 			<h3 className="mb-5 text-white"> Por Favor escaneio o QR code </h3>
 			<Scanner
 				styles={{
-					container: { alignContent: 'center', height: '40vw', width: '40vw' },
+					container: { alignContent: 'center', height: '60vw', width: '60vw' },
 					// video: { height: 20 },
 					finderBorder: 2,
 				}}
-				onScan={(result) => handleValue(result[0].rawValue)}
+				onScan={(result) => handlerQr(result[0].rawValue)}
 			/>
 			;
 		</div>
