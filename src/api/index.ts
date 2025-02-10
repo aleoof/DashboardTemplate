9aeca2ce9c2eab54d7cb1151ce4dbacdd91c59fb
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-02-10 11:15:29 -0300

    try to use .env file

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const secret = localStorage.getItem('token');
 
 export const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: import.meta.env.VITE_API_URL || 'https://api.shartech.com.br/',
 	headers: { Authorization: `Bearer ${secret}` },
 });
