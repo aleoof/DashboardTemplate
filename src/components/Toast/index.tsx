78b2940df642ddea4f5cad314430388501808be8
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-02-14 14:29:06 -0300

    update the toast error

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -7,7 +7,7 @@ export default function Toast({ success }: { success?: boolean }) {
 					<p className="text-white">Salvo com sucesso</p>
 				</div>
 			) : (
-				<div className="position-fixed card bg-success top-0 end-0 w-25 m-3 p-2 text-white">
+				<div className="position-fixed card bg-danger top-0 end-0 w-25 m-3 p-2 text-white">
 					<strong>Erro</strong>
 					<p className="text-white">Erro ao criar</p>
 				</div>
