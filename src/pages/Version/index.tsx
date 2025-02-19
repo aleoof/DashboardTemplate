32b5090cd2c9357237c701a8169de1ee32e24531
Edson Rodrigues <shartech.solucoes@gmail.com>
2025-02-19 10:33:57 -0300

    Inclusão de Versão do sistema

--- src/pages/Version/index.tsx
+++ src/pages/Version/index.tsx
@@ -6,6 +6,19 @@ export default function Version() {
 				<div className="col-8">
 					<div className="card">
 						<div className="card-body">
+							<h5>Release 1.1</h5>
+							<p>Update 17 de fevereiro de 2026, 22:12AM</p>
+
+							<h5>Novas Funções implementadas</h5>
+							<p>- Sistema de duplicação de OS </p>
+							<p>- Relatório OS por dia </p>
+							<p>- Inclusão campo número de protocolo </p>
+
+							<h5>Correções</h5>
+							<p>- Exclusão de kit da OS</p>
+
+							<hr className='m-5'/>
+
 							<h5>Release 1.0</h5>
 							<p>Update 07 de fevereiro de 2026, 10:12AM</p>
 
