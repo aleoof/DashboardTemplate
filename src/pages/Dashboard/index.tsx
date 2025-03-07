23457aa863d5185a619250f97d93f6d97c08ea70
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-03-07 19:47:45 -0300

    implement map in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,9 +4,26 @@ import {
 	BsTools,
 } from 'react-icons/bs';
 import ListItemOrdersDash from '../../components/ListItem/OrdersDash';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import { api } from '../../api';
+import {
+	GoogleMap,
+	Marker,
+	MarkerF,
+	useJsApiLoader,
+} from '@react-google-maps/api';
+import axios from 'axios';
+
+const containerStyle = {
+	width: '100%',
+	height: '400px',
+};
+
+const center = {
+	lat: -25.315827,
+	lng: -49.287565,
+};
 
 export default function Dashboard() {
 	const [orders, setOrders] = useState<
@@ -67,10 +84,63 @@ export default function Dashboard() {
 		}));
 	};
 
+	const { isLoaded } = useJsApiLoader({
+		id: 'google-map-script',
+		googleMapsApiKey: 'AIzaSyCLYeK1ksPfWhPxgZZ687Vdi-eDFLFRCr0',
+	});
+
+	const [map, setMap] = useState(null);
+	const [pins, setPins] = useState<{ geo: any; os: string }[]>([]);
+
+	const onLoad = useCallback(
+		(map) => {
+			if (pins.length > 0) {
+				map.setZoom(12);
+				setMap(map);
+			}
+		},
+		[pins]
+	);
+
+	const onUnmount = useCallback(() => {
+		setMap(null);
+	}, [map]);
+
+	const getGeolocation = async () => {
+		const getLocationsOrders = orders.map(async (order) => {
+			const address = `${order.order.address} ${order.order.neighborhood} ${order.order.city}`;
+
+			const formatedAddress = address
+				.replaceAll('  ', ' ')
+				.replaceAll(' ', '%20');
+
+			const response = await axios.get(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${formatedAddress}&key=AIzaSyCLYeK1ksPfWhPxgZZ687Vdi-eDFLFRCr0`
+			);
+
+			return { geolocation: response.data, os: order.order.qr_code };
+		});
+		const geolocation = await Promise.all(getLocationsOrders);
+
+		const formatedGeo = geolocation.map((loc) => ({
+			geo: loc.geolocation.results[0].geometry.location,
+			os: loc.os,
+		}));
+
+		console.log(formatedGeo);
+
+		setPins(formatedGeo);
+	};
 	useEffect(() => {
 		getOrders();
 		getDashboardData();
 	}, []);
+
+	useEffect(() => {
+		if (orders.length > 0) {
+			getGeolocation();
+		}
+	}, [orders]);
 
 	return (
 		<div>
@@ -144,6 +214,18 @@ export default function Dashboard() {
 					<div className="card">
 						<div className="card-body">
 							<p className="card-title">Atendimentos realizado hoje</p>
+							{isLoaded && pins.length > 0 && (
+								<GoogleMap
+									mapContainerStyle={containerStyle}
+									center={center}
+									onLoad={onLoad}
+									onUnmount={onUnmount}
+								>
+									{pins.map((pin) => (
+										<MarkerF position={pin.geo} label={`${pin.os}`} />
+									))}
+								</GoogleMap>
+							)}
 						</div>
 					</div>
 				</div>
@@ -167,36 +249,36 @@ export default function Dashboard() {
 							<p className="card-title">OS do dia</p>
 						</div>
 
-							<table className="w-100">
-								<thead>
-									<tr>
-										<th className="text-start">Numero OS</th>
-										<th className="text-center">Hora</th>
-										<th className="text-start">Endereço</th>
-										<th className="text-start">Bairro</th>
-										<th className="text-start">Cidade</th>
-										<th className="text-start">Status</th>
-										<th>Ver</th>
-									</tr>
-								</thead>
-								<tbody>
-									{orders.map((order) => (
-										<>
-											<ListItemOrdersDash
-												key={order.order.id}
-												qrcode={order.order.qr_code}
-												register={order.order.registerDay}
-												id={order.order.id}
-												status={order.order.status}
-												address={order.order.address}
-												neighborhood={order.order.neighborhood}
-												city={order.order.city}
-											/>
-											<hr />
-										</>
-									))}
-								</tbody>
-							</table>
+						<table className="w-100">
+							<thead>
+								<tr>
+									<th className="text-start">Numero OS</th>
+									<th className="text-center">Hora</th>
+									<th className="text-start">Endereço</th>
+									<th className="text-start">Bairro</th>
+									<th className="text-start">Cidade</th>
+									<th className="text-start">Status</th>
+									<th>Ver</th>
+								</tr>
+							</thead>
+							<tbody>
+								{orders.map((order) => (
+									<>
+										<ListItemOrdersDash
+											key={order.order.id}
+											qrcode={order.order.qr_code}
+											register={order.order.registerDay}
+											id={order.order.id}
+											status={order.order.status}
+											address={order.order.address}
+											neighborhood={order.order.neighborhood}
+											city={order.order.city}
+										/>
+										<hr />
+									</>
+								))}
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
